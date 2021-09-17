6a4f72a7fb8e2e42a33f2b64a015548c3d2198cb
Obosrashka69 <skorikovdanila@mail.ru>
2021-09-18 09:52:26 +1000

    version 5

--- models/authModels.js
+++ models/authModels.js
@@ -3,9 +3,7 @@ const Schema = mongoose.Schema
 
 const ticketSchema = new Schema({
     NumberOfTicket: {type: String, required: true},
-    train: {type: Array, required: true},
     user: {type: Array, required: true},
-    price: {type: String, required: true}
 })
 
 module.exports = mongoose.model('ticket', ticketSchema)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,29 @@
 const express = require('express')
-const controller = require('../controllers/auth')
 const router = express.Router()
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const authModels = require("../models/authModels")
+const keys = require("../config/default.json")
 
 // Адрес роута: localhost:5000/api/auth/login
-router.post('/login', controller.login)
+router.post('/login', async (req, res) => {
+    try {
+        const candidate = await authModels.findOne({NumberOfTicket: req.body.NumberOfTicket})
+        if(candidate) {
+            const api = ({
+                objectId: candidate._id,
+                NumberOfTicket: candidate.NumberOfTicket,
+                user: candidate.user,
+            })
+            res.status(200).json({api: api})
+        } else {
+            res.status(404).json({message: "Такого билета не существует."})
+        }
+    } catch (e) {
+        res.status(500).json({message: "Упс... Что-то пошло не так."})
+    }
+})
 
 module.exports = router
