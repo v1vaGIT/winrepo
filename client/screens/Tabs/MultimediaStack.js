c7519fa60c2b7540ef113433c248c337f6b5dc13
Timur Shurygin <timur.krut27@mail.ru>
2021-09-18 18:06:02 +1000

    mod3

--- client/screens/Tabs/MultimediaStack.js
+++ client/screens/Tabs/MultimediaStack.js
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { View, Text, FlatList, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, ScrollView, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { multimedia_data } from "../../data/multimedia_data";
 
 const MultimediaItemCard = ({ route }) => {
@@ -12,24 +12,35 @@ const MultimediaItemCard = ({ route }) => {
 const Multimedia = ({ navigation }) => {
   const renderItem = ({ item }) => {
     return (
-      <View>
-        <Text>{item.title}</Text>
-        <ScrollView horizontal>
+      <View style={styles.container}>
+        <Text style={styles.mediaTitle}>{item.title}</Text>
+          <ScrollView horizontal>
           {item.options.map(x => {
             return (
+              <View style={styles.mediaBox}> 
               <TouchableOpacity onPress={() => navigation.navigate('media-item-card', {
                 title: x.title
               })}>
-                <Text>{x.title}</Text>
+                <Text style={styles.x_title}>{x.title}</Text>
               </TouchableOpacity>
+              </View>
             )
           })}
         </ScrollView>
+                  
     </View>
     )
   }
   return (
     <View>
+      <View style={styles.header}>
+        <View style={styles.headerOne}>
+          <Image source={require('../../assets/news/Logo.jpg')} style={styles.logoHeader}/>
+        </View>
+        <View style={styles.headerTwo}>
+          <Text style={styles.textHeader}>Мультимедиа</Text>
+        </View>
+      </View>
       <FlatList 
         data={multimedia_data}
         renderItem={renderItem}
@@ -47,3 +58,51 @@ export const MultimediaStack = () => {
     </MediaStack.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  
+  header: {
+    display: 'flex',
+    position: 'absolute',
+    top: 0,
+    start: 0,
+    flexDirection: "row"
+  },
+  headerOne: {
+    flex: 1, 
+    alignItems: 'center',
+  },
+  headerTwo: {
+    flex: 5,
+    alignItems: 'flex-start'
+  },
+  logoHeader: {
+    width: 30,
+    height: 30,
+    marginTop: '74%',
+  },
+  textHeader: {
+    fontFamily: 'GothamPro-Medium',
+    fontSize: 19,
+    marginTop: '16%',
+  },
+  //----------------------------------------------------
+  container: {
+    marginTop: '20%'
+  },
+  mediaTitle: {
+    fontSize: 20,
+    fontFamily: 'GothamPro-Medium',
+    textAlign:'center',
+    marginTop: '7%'
+  },
+  mediaBox: {
+    fontSize: 25
+  },
+  x_title: {
+    fontSize: 25,
+    padding: 10,
+    fontFamily: 'GothamPro-Medium'
+
+  }
+})
