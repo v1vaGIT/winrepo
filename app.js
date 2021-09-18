d31af6bcd9496a5c1a2f8e2e8274f329b357b1af
Timur Shurygin <timur.krut27@mail.ru>
2021-09-18 11:52:33 +1000

    update

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ const PORT = config.get('port') || 5000
 async function start() {
     try {
         await mongoose.connect(config.get('mongoUri'), {
-
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         })
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
     } catch (e) {
         console.log('Упс... Что-то пошло не так.', e.message)
         process.exit(1)
@@ -21,4 +24,3 @@ async function start() {
 
 start()
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
