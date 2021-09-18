99f584389cfe4a9b957534bc97de67206aca28fe
Obosrashka69 <skorikovdanila@mail.ru>
2021-09-18 11:32:16 +1000

    version 6

--- models/authModels.js
+++ models/authModels.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const ticketSchema = new Schema({
     NumberOfTicket: {type: String, required: true},
     user: {type: Array, required: true},
+    links: [{ type: mongoose.Types.ObjectId, ref: 'Link'}]
 })
 
 module.exports = mongoose.model('ticket', ticketSchema)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
 const authModels = require("../models/authModels")
 const keys = require("../config/default.json")
 
