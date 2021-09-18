b396bac265a87e6f4eacb8d8d39f0492c40a22a4
Obosrashka69 <skorikovdanila@mail.ru>
2021-09-18 16:34:29 +1000

    version 8

--- client/screens/TabsScreens.js
+++ client/screens/TabsScreens.js
@@ -6,17 +6,78 @@ import { MarketnCart } from "./Tabs/MarketnCart";
 import { MultimediaStack } from "./Tabs/MultimediaStack";
 import { News } from "./Tabs/NewsStack";
 import { Train } from "./Tabs/Train";
+import {Image } from 'react-native';
 
 const Tabs = createBottomTabNavigator()
 
 export const TabsScreens = () => {
   return (
     <Tabs.Navigator>
-      <Tabs.Screen name='train' component={Train}/>
-      <Tabs.Screen name='news' component={News} options={{headerShown: false}}/>
-      <Tabs.Screen name='market' component={MarketnCart} options={{headerShown: false}}/>
-      <Tabs.Screen name='multimedia' component={MultimediaStack} />
-      <Tabs.Screen name='feedback' component={Feedback} />
+      <Tabs.Screen name=' ' component={Train} options={{tabBarIcon: ({focused}) => (
+        <View style={{alignItems:'center', justifyContent:'center', top: 7}}>
+          <Image 
+            source={require('.././assets/tabsIcons/icons8-поезд-80.png')} 
+            resizeMode = 'contain'
+            style={{
+              width: 30,
+              height: 30,
+              tintColor: focused ? "e32f45" : "#748c94"
+            }}
+          />
+        </View>
+      ),}}/>
+    <Tabs.Screen name='  ' component={News} options={{headerShown: false, tabBarIcon: ({focused}) => (
+      <View style={{alignItems:'center', justifyContent:'center', top: 7}}>
+        <Image 
+          source={require('.././assets/tabsIcons/icons8-новости-50.png')} 
+          resizeMode = 'contain'
+          style={{
+            width: 30,
+            height: 30,
+            tintColor: focused ? "e32f45" : "#748c94"
+          }}
+        />
+      </View>
+    ),}}/>
+    <Tabs.Screen name='   ' component={MarketnCart} options={{headerShown: false, tabBarIcon: ({focused}) => (
+      <View style={{alignItems:'center', justifyContent:'center', top: 7}}>
+        <Image 
+          source={require('.././assets/tabsIcons/icons8-продуктовый-магазин-50.png')} 
+          resizeMode = 'contain'
+          style={{
+            width: 30,
+            height: 30,
+            tintColor: focused ? "e32f45" : "#748c94"
+          }}
+        />
+      </View>
+    ),}}/>
+    <Tabs.Screen name='     ' component={MultimediaStack} options={{tabBarIcon: ({focused}) => (
+      <View style={{alignItems:'center', justifyContent:'center', top: 7}}>
+        <Image 
+          source={require('.././assets/tabsIcons/icons8-видео-плейлист-50.png')} 
+          resizeMode = 'contain'
+          style={{
+            width: 30,
+            height: 30,
+            tintColor: focused ? "e32f45" : "#748c94"
+          }}
+        />
+      </View>
+    ),}}/>
+    <Tabs.Screen name='      ' component={Feedback} options={{tabBarIcon: ({focused}) => (
+      <View style={{alignItems:'center', justifyContent:'center', top: 7}}>
+        <Image 
+          source={require('.././assets/tabsIcons/icons8-комментарии-50.png')} 
+          resizeMode = 'contain'
+          style={{
+            width: 30,
+            height: 30,
+            tintColor: focused ? "e32f45" : "#748c94"
+          }}
+        />
+        </View>
+      ),}}/>
     </Tabs.Navigator>
   )
 }
